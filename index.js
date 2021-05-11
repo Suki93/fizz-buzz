3d8351f663b9df72e8154848c708a773033e83aa
Nemanja <nemanjasurudzic93@gmail.com>
2021-05-11 15:11:18 +0200

    just roll

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const input = document.querySelector(".numberInput");
 const para = document.querySelector("p");
 
+//function fizz(num) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+  //      return "FIZZ-BUZZ"
+    //} else if (num % 3 === 0) {
+      //  return "FIZZ";
+   // } else if (num % 5 === 0) {
+   //     return "BUZZ";
+   // }  else {
+     //   return num;
+//}
+//}
+
 function fizz(num) {
-     if (num % 3 === 0 && num % 5 === 0) {
-        return "FIZZ-BUZZ"
-    } else if (num % 3 === 0) {
-        return "FIZZ";
-    } else if (num % 5 === 0) {
-        return "BUZZ";
-    }  else {
-        return num;
+    for(i = 1; i <= num; i++) {
+        if (num % 3 === 0 && num % 5 === 0) {
+            return "FIZZ-BUZZ"
+        } else if (num % 3 === 0) {
+            return "FIZZ";
+        } else if (num % 5 === 0) {
+            return "BUZZ";
+        }  else {
+            return num;
+        }
     }
 }
+
 
 input.onchange = function() {
     const num = input.value;
