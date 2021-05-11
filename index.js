7817d98abbe175530d588e03340c742d60819f5b
Nemanja <nemanjasurudzic93@gmail.com>
2021-05-11 13:50:38 +0200

    just roll with it

---
+++ index.js
@@ -0,0 +1,23 @@
+const input = document.querySelector(".numberInput");
+const para = document.querySelector("p");
+
+function fizz(num) {
+     if (num % 3 === 0 && num % 5 === 0) {
+        return "FIZZ-BUZZ"
+    } else if (num % 3 === 0) {
+        return "FIZZ";
+    } else if (num % 5 === 0) {
+        return "BUZZ";
+    }  else {
+        return num;
+    }
+}
+
+input.onchange = function() {
+    const num = input.value;
+    if (isNaN(num)) {
+        para.textContent = "You need to put in a number!";
+    } else {
+        para.textContent = fizz(num);
+    }
+}
